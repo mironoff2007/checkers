0be95dd89a6ef6d24bbad918d1ac1beba55caab5
mironoff2007 <mironoff2007@rambler.ru>
2021-10-31 14:24:41 +0300

    Draw board

---
+++ app/src/main/java/com/mironov/checkers/FlowLayout.kt
@@ -0,0 +1,99 @@
+package com.mironov.checkers
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.ViewGroup
+import kotlin.math.max
+
+class FlowLayout : ViewGroup {
+    private var lineHeightSpace = 0
+    private var nextLine=false
+    private val numberViewsInLine=8
+
+    class LayoutParams
+    /**
+     * @param horizontal_spacing Pixels between items, horizontally
+     * @param vertical_spacing   Pixels between items, vertically
+     */(var horizontal_spacing: Int, var vertical_spacing: Int) :
+        ViewGroup.LayoutParams(0, 0) {
+    }
+
+    constructor(context: Context?) : super(context)
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        assert(MeasureSpec.getMode(widthMeasureSpec) != MeasureSpec.UNSPECIFIED)
+        val width = MeasureSpec.getSize(widthMeasureSpec) - paddingLeft - paddingRight
+        var height = MeasureSpec.getSize(heightMeasureSpec) - paddingTop - paddingBottom
+        val count = childCount
+        var lineHeightSpace =0
+        var xpos = paddingLeft
+        var ypos = paddingTop
+        val childHeightMeasureSpec: Int = if (MeasureSpec.getMode(heightMeasureSpec) == MeasureSpec.AT_MOST) {
+            MeasureSpec.makeMeasureSpec(height, MeasureSpec.AT_MOST)
+        } else {
+            MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED)
+        }
+        for (i in 0 until count) {
+            val child = getChildAt(i)
+            if (child.visibility != GONE) {
+                val lp = child.layoutParams as LayoutParams
+                child.measure(
+                    MeasureSpec.makeMeasureSpec(width, MeasureSpec.AT_MOST),
+                    childHeightMeasureSpec
+                )
+                val childWidth = child.measuredWidth
+                lineHeightSpace =
+                    max(lineHeightSpace, child.measuredHeight + lp.vertical_spacing)
+
+                if (i%numberViewsInLine==0) {
+                    xpos = paddingLeft
+                    ypos += lineHeightSpace
+                    nextLine=false
+                }
+                xpos += childWidth + lp.horizontal_spacing
+            }
+        }
+        this.lineHeightSpace = lineHeightSpace
+        if (MeasureSpec.getMode(heightMeasureSpec) == MeasureSpec.UNSPECIFIED) {
+            height = ypos + lineHeightSpace
+        } else if (MeasureSpec.getMode(heightMeasureSpec) == MeasureSpec.AT_MOST) {
+            if (ypos + lineHeightSpace < height) {
+                height = ypos + lineHeightSpace
+            }
+        }
+        setMeasuredDimension(width, height)
+    }
+
+    override fun generateDefaultLayoutParams(): ViewGroup.LayoutParams {
+        return LayoutParams(1, 1) // default of 1px spacing
+    }
+
+    override fun checkLayoutParams(p: ViewGroup.LayoutParams): Boolean {
+        return p is LayoutParams
+    }
+
+    override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
+        val count = childCount
+        val width = r - l
+        var xpos = paddingLeft
+        var ypos = paddingTop
+        for (i in 0 until count) {
+            val child = getChildAt(i)
+            if (child.visibility != GONE) {
+                val childWidth = child.measuredWidth
+                val childHeight = child.measuredHeight
+                val lp = child.layoutParams as LayoutParams
+
+                if (i%numberViewsInLine==0) {
+                    xpos = paddingLeft
+                    ypos += lineHeightSpace
+                    nextLine=false
+                }
+
+                child.layout(xpos, ypos, xpos + childWidth, ypos + childHeight)
+                xpos += childWidth + lp.horizontal_spacing
+            }
+        }
+    }
+}
--- app/src/main/java/com/mironov/checkers/MainActivity.kt
+++ app/src/main/java/com/mironov/checkers/MainActivity.kt
@@ -1,12 +1,79 @@
 package com.mironov.checkers
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import android.util.DisplayMetrics
+import androidx.core.view.marginLeft
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var flowLayout: FlowLayout
+
+    private var screenWidth=0
+
+    private fun findViews() {
+        flowLayout = findViewById(R.id.flowLayout)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val displayMetrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(displayMetrics)
+        screenWidth = displayMetrics.widthPixels
+
+        findViews()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        addLayouts()
+    }
+
+    @SuppressLint( "SetTextI18n", "InflateParams", "UseCompatLoadingForDrawables")
+    private fun addLayouts() {
+
+        flowLayout.removeAllViews()
+
+
+        val margin=flowLayout.marginLeft
+        val marginDp=dpToPixel(margin)
+
+        val tileSize=(screenWidth-2*marginDp)/ 8
+        var j=0
+        for (i in 0..63) {
+            if(i%8==0){j++}
+
+            val selected = booleanArrayOf(false)
+            val view: View = this.layoutInflater.inflate(R.layout.tile, null)
+            val imageView = view.findViewById<View>(R.id.tileImage) as ImageView
+
+            imageView.layoutParams.height=tileSize
+            imageView.layoutParams.width=tileSize
+
+            //Set spacing here
+            view.layoutParams=FlowLayout.LayoutParams(1,1)
+
+            when {
+                (i+j) % 2 == 0 -> imageView.setImageDrawable(resources.getDrawable(R.drawable.ic_cell_dark))
+                (i+j) % 2 != 0 -> imageView.setImageDrawable(resources.getDrawable(R.drawable.ic_cell_light))
+            }
+
+            flowLayout.addView(view)
+
+            view.setOnClickListener {
+                selected[0] = !selected[0]
+            }
+        }
+    }
+    fun dpToPixel( dpValue: Int): Int {
+        val scale: Float = resources.getDisplayMetrics().density
+        return (dpValue * scale + 0.5f).toInt()
     }
 }
